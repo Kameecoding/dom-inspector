2241e523a83361e357a40d553719686cd1061f32
Andrej Kovac <Kameecoding@users.noreply.github.com>
2017-04-10 06:34:45 +0200

    Update dom-inspector.js

--- dom-inspector.js
+++ dom-inspector.js
@@ -14,10 +14,14 @@ const domListId = "cz.vutbr.fit.dom-list";
 /* CSS and HTML constants */ 
 const offset = 15;
 const innerOffset = 9;
-const start = "<";
-const end = ">";
-const singleEnd = "/>"
-const endStart = "</"
+const start = document.createElement('span'); 
+	  start.textContent = "<";
+const end = document.createElement('span');
+	  end.textContent = ">";
+const singleEnd = document.createElement('span');
+	  singleEnd.textContent = "/>";
+const endStart = document.createElement('span');
+	  endStart.textContent = "</";
 
 /**
  * Enumerate nodetypes for easier code readability / avoid magic constants
@@ -72,8 +76,8 @@ class TreeNode {
 
     //Add Element to the Dom panel
     appendToDom(elem) {
-        if (this.ul) {
-            this.ul.appendChild(elem);
+        if (this.parent.ul) {
+            this.parent.ul.appendChild(elem);
         } else {
             this.parent.domElement.appendChild(elem);    
         }
@@ -163,20 +167,29 @@ function buildTree(currentNode) {
     var children = currentNode.bodyElement.childNodes;
     for (let i = 0; i<children.length; i++) {
         //create node in the tree
-        if (children[i].textContent == "\n") {
+        if (children[i].nodeType == NodeTypes.TEXT_NODE && children[i].textContent.trim() == "") {
             continue;
         }
         let newNode = new TreeNode(currentNode, [], children[i], null);
         var type = newNode.bodyElement.nodeType;
-        if (type == NodeTypes.ELEMENT_NODE || type == NodeTypes.DOCUMENT_NODE) {
+        if (type == NodeTypes.ELEMENT_NODE 
+                || type == NodeTypes.DOCUMENT_NODE || type == NodeTypes.TEXT_NODE) {
         	let call = buildElement(newNode);
             //Recursive call
             if (call) {
                 buildTree(newNode);
             }
-        } else if (type == NodeTypes.TEXT_NODE) {
-        	buildText(newNode);
         }
+    }
+    if (currentNode.ul) {
+    	let li = document.createElement('li');
+    	currentNode.ul.appendChild(li);
+    	li.appendChild(endStart.cloneNode(true));
+    	let tag = document.createElement('span');
+        tag.textContent = currentNode.bodyElement.tagName.toLowerCase();;
+        tag.className="html";
+        li.appendChild(tag);
+        li.appendChild(end.cloneNode(true));
     }
 }
 
@@ -190,49 +203,74 @@ function buildElement(newNode) {
 	let li = document.createElement('li');
     newNode.appendToDom(li);
     newNode.domElement = li;
-    let span = document.createElement('span');
-    li.appendChild(span);
-    //create visual representation of the html code in the inspector
-    span.textContent = "<" + newNode.bodyElement.tagName.toLowerCase();
-    //Add attributes and values
-    let atts = newNode.bodyElement.attributes;
-    if (atts) {
-        for (let i = 0; i < atts.length; i++){
-            let att = atts[i];
-            span.textContent += ' ';
-            span.textContent += att.nodeName + '="' + att.nodeValue;
-        }
-    }
-    if (newNode.bodyElement.childNodes.length == 1 
-            && newNode.bodyElement.childNodes[0].nodeType == NodeTypes.TEXT_NODE) {
-        span.textContent += '>' + newNode.bodyElement.childNodes[0].textContent;
-        span.textContent += '</' + newNode.bodyElement.tagName.toLowerCase() + '>';
+    
+
+    var type = newNode.bodyElement.nodeType;
+    if (type == NodeTypes.TEXT_NODE) {
+        let span = document.createElement('span');
+        li.appendChild(span);
+        span.textContent = newNode.bodyElement.textContent;
         return false;
-    } else if (newNode.bodyElement.childNodes.length > 1) {
-        let ul = document.createElement('ul');
-        li.appendChild(ul);
-        newNode.setCurrentList(ul);
-    } else {
-        newNode.setCurrentList(null);
+    } else if (type == NodeTypes.ELEMENT_NODE || type == NodeTypes.DOCUMENT_NODE) { 
+        li.appendChild(start.cloneNode(true));
+        let tag = document.createElement('span');
+        tagName = newNode.bodyElement.tagName.toLowerCase();
+        tag.textContent = tagName;
+        tag.className="html";
+        li.appendChild(tag);
+       
+        //Add attributes and values
+        let atts = newNode.bodyElement.attributes;
+        if (atts) {
+            for (let i = 0; i < atts.length; i++){
+                let att = atts[i];
+                //li.textContent += ' ';
+                let attSpan = document.createElement('span');
+                li.appendChild(attSpan);
+                attSpan.textContent = ' ' + att.nodeName + '=';
+                attSpan.className = "attribute";
+                //li.textContent += '=' 
+                let attValSpan = document.createElement('span');
+                li.appendChild(attValSpan);
+                attValSpan.textContent = '"' + att.nodeValue + '"';
+                attValSpan.className = "attributeValue";
+            }
+        }
+
+
+        if (newNode.bodyElement.childNodes.length == 1 
+                && newNode.bodyElement.childNodes[0].nodeType == NodeTypes.TEXT_NODE) {
+        	li.appendChild(end.cloneNode(true));
+            let text = document.createElement('span');
+            text.textContent += newNode.bodyElement.childNodes[0].textContent;
+            li.appendChild(text);
+            li.appendChild(endStart.cloneNode(true));
+            li.appendChild(tag.cloneNode(true));
+            li.appendChild(end.cloneNode(true));
+            return false;
+        } else if (newNode.bodyElement.childNodes.length > 1) {
+            li.appendChild(end.cloneNode(true));
+            let ul = document.createElement('ul');
+            li.appendChild(ul);
+            newNode.setCurrentList(ul);
+        } else {
+            if (newNode.ul) {
+                li.appendChild(endStart.cloneNode(true));
+                li.appendChild(tag.cloneNode(true));
+                li.appendChild(end.cloneNode(true));
+            } else{
+                let closeSpan = document.createElement('span');
+                closeSpan.textContent = '/>';
+                li.appendChild(closeSpan);
+            } 
+            newNode.setCurrentList(null);
+        }
+
+        
+
+        return true;
     }
-
-    return true;
-    //newNode.domElement.style.paddingLeft = newNode.getParentOffset() + offset + 'px';
-    //newNode.domElement.className = "toggle";  
 }
-
-/**
- * Function for handling Text content
- */
-function buildText(newNode) {
-    let li = document.createElement('li');
-    newNode.appendToDom(li);
-    newNode.domElement = li;
-    let span = document.createElement('span');
-    li.appendChild(span);
-	span.textContent = newNode.bodyElement.textContent;
-}
-
 
 /** Function that count occurrences of a substring in a string;
  * @param {String} string               The string
